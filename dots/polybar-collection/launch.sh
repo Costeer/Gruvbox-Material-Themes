f03e6a903464aa6662c2d19d4d535d30497803bd
Costeer <142180709+Costeer@users.noreply.github.com>
2023-12-04 17:09:18 +0100

    Delete dots directory

--- dots/polybar-collection/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THEME="gruvbox"
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=$(dirname $0)/themes/$THEME/config.ini
-polybar main -c $CONFIG_DIR &
